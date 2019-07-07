e04c33ac15391c885fcf00822f2136dccc40ec38
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-07 13:40:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,4 @@
-#push me
+#using_push takes in two arguments, an array and a string and adds that string to the end of the array using the push method
+def using_push(array, string)
+  array.push(string)
+end
