a439c3755d223c79c1c8812f3948015232030fca
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-07 13:46:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,14 +1,14 @@
 #using_push takes in two arguments, an array and a string and adds that string to the end of the array using the push method
-def using_push(Array, String)
-  Array.push(String)
+def using_push(array, string)
+  array.push(string)
 end
 
 #using_unshift takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method
-def using_unshift(Array, String)
-  Array.unshift(String)
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
 #using_pop takes in argument of an array and uses the pop method to remove the last element from the array and return that element
-def using_pop(Array)
+def using_pop(array)
   
 end
