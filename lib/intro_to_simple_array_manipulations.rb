662caeac1eccc55e848117cae19ef381974fe3a6
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-07-07 14:01:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,6 +48,7 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at (array, integer)
-  array.delete
+def using_delete_at (array, int)
+  array.delete_at(int)
 end
+
